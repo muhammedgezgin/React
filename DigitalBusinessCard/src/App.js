e55e6caeab693108e793d7587a57243a2666a190
Muhammed Gezgin <101412521+muhammedgezgin@users.noreply.github.com>
2022-07-11 20:19:58 +0300

    Update App.js

--- DigitalBusinessCard/src/App.js
+++ DigitalBusinessCard/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer'
-import './styles/App.css'
+import './styles/style.css'
 
 
 const App = () => {
